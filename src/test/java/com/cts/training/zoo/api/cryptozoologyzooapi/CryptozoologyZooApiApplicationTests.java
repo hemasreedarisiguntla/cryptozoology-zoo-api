b95350916938827fc2dcc7b75e32ffd3d8d9167c
Hemasree Darisiguntla <sreed2503@gmail.com>
2021-01-09 14:07:14 -0500

    Initial commit

---
+++ src/test/java/com/cts/training/zoo/api/cryptozoologyzooapi/CryptozoologyZooApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.training.zoo.api.cryptozoologyzooapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptozoologyZooApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
